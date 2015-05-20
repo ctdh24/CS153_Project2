3de4732e9887496433b3267d23f141d4cf744d19
Jeffrey C <jeff2435@gmail.com>
2015-05-20 12:30:29 -0700

    pid_t doesn't exist, so removing function prototype

--- pintos/pintos/src/userprog/syscall.h
+++ pintos/pintos/src/userprog/syscall.h
@@ -23,7 +23,6 @@ void syscall_init (void);
 void halt (void);
 void exit (int status);
 //pid_t exec (const char *cmd_line) ;
-int wait(pid_t pid);
 //int wait (pid_t pid) ;
 bool create (const char *file, unsigned initial_size) ;
 bool remove (const char *file) ;
