663191d9f7a2b1db4a224bcdd7a943d897250e06
Jeff Chen <jchen086@sledge.cs.ucr.edu>
2015-05-22 10:55:29 -0700

    oopsies redeclaration

--- pintos/pintos/src/userprog/syscall.c
+++ pintos/pintos/src/userprog/syscall.c
@@ -20,12 +20,6 @@
 
 struct lock filesys_lock;
 
-struct process_file {
-  struct file *file;
-  int fd;
-  struct list_elem elem;
-};
-
 int process_add_file (struct file *f);
 struct file* process_get_file (int fd);
 
