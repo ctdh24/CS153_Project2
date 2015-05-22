f3610c20bcc24874e1085b59512bc56cf3a01d70
Jeffrey C <jeff2435@gmail.com>
2015-05-22 10:41:31 -0700

    forgot some constants

--- pintos/pintos/src/userprog/syscall.h
+++ pintos/pintos/src/userprog/syscall.h
@@ -3,7 +3,12 @@
 
 #include "threads/synch.h"
 
-struct lock filesys_lock;
+#define CLOSE_ALL -1
+#define ERROR -1
+
+#define NOT_LOADED 0
+#define LOAD_SUCCESS 1
+#define LOAD_FAIL 2
 
 struct child_process {
   int pid;
@@ -15,25 +20,13 @@ struct child_process {
   struct list_elem elem;
 };
 
-void syscall_init (void);
-
-void halt (void);
-void exit (int status);
-//pid_t exec (const char *cmd_line) ;
-//int wait (pid_t pid) ;
-bool create (const char *file, unsigned initial_size) ;
-bool remove (const char *file) ;
-int open (const char *file) ;
-int filesize (int fd) ;
-int read (int fd, void *buffer, unsigned size) ;
-int write (int fd, const void *buffer, unsigned size) ;
-void seek (int fd, unsigned position) ;
-unsigned tell (int fd) ;
-void close (int fd) ;
-
 struct child_process* add_child_process (int pid);
 struct child_process* get_child_process (int pid);
 void remove_child_process (struct child_process *cp);
 void remove_child_processes (void);
+
+void process_close_file (int fd);
+
+void syscall_init (void);
 
 #endif /* userprog/syscall.h */
